9e245e5f83a2055157c4cc21aa9a132fa6bdd92d
Florian <florian.schuch@gmx.net>
2017-04-06 17:04:12 +0200

    helloooooo world

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("Hello World")
+        print("Hellooooo World")
     }
 
     override func didReceiveMemoryWarning() {
